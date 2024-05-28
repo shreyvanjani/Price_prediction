1b38843c18723d4cefa8109af80b7bed60da7f3d
Shreyansh vanjani <98659367+shreyvanjani@users.noreply.github.com>
2024-05-29 03:01:56 +0530

    Alpha1.22

--- server/server.py
+++ server/server.py
@@ -1,4 +1,7 @@
+from io import StringIO
+import pandas as pd 
 from flask import Flask, jsonify, request
+import requests
 import util
 
 
@@ -33,8 +36,40 @@ def predict_home_price():
     response.headers.add('access-control-allow-origin', '*')
     return response
     
-    
+@app.route('/fetch-pincode-data', methods=['GET','POST'])
+def fetch_pincode_data():
+    # Get the pincode from the query parameters
+    pincode = request.args.get('pincode')
+    if not pincode:
+        return jsonify({"error": "PIN code is missing"}), 400
+
+    # API endpoint with the provided PIN code
+    api_url = f"https://api.data.gov.in/resource/5c2f62fe-5afa-4119-a499-fec9d604d5bd?api-key=579b464db66ec23bdd0000011c2b8a38f6a1409e4b0476daf248ecc8&format=csv&limit=4&filters%5Bpincode%5D={pincode}"
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+
+        data = StringIO(response.text)
+        df = pd.read_csv(data)
+
+        # Extract longitude and latitude values
+        long = df['longitude'].values
+        lat = df['latitude'].values
+        
+        # Create a list of coordinate pairs
+        coordinates = list(zip(lat, long))
+
+        # Convert the coordinates to a list of dictionaries for JSON serialization
+        coordinates_list = [{"latitude": lat, "longitude": lon} for lat, lon in coordinates]
+
+        return jsonify({"coordinates": coordinates_list})
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+ 
 if __name__ == "__main__":
     print("starting server")
     util.load_artifacts()
-    app.run()
+    app.run(debug = True)
